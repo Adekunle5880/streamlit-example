35da06eee4a9c2a40973f4c4356db422fdfb2140
Adekunle5880 <49585030+Adekunle5880@users.noreply.github.com>
2023-09-02 20:14:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,43 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
+import numpy as np
+import pickle 
+import sklearn
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.header("Compressive Strength Prediction")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.markdown('''
+Input the following details to predict the compressive strength
+''')
 
-    points_per_turn = total_points / num_turns
+model = pickle.load(open('model.pickle','rb'))
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+Age= st.number_input('Enter the age in days')
+Weight_Aggregate= st.number_input('Enter the weight of fine aggregate')
+Weight_Cement = st.number_input('Enter the weight of cement')
+Percent_TMD= st.number_input('Enter the percentage of TMD')
+Weight_TMD = st.number_input('Enter the weight of TMD')
+Weight_Coarse = st.number_input('Enter the weight of coarse aggregate')
+Compaction = st.number_input('Enter the  compaction count')
+Slump = st.number_input('Enter the slump result')
+Cube_Density = st.number_input ('Enter the density of cube')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+data = {'Age (days)': Age,
+        'weight of fine aggregrate':Weight_Aggregate,
+	'weight of cement':Weight_Cement,
+	'percentage of TMD':Percent_TMD,
+	'weight of coarse aggregate':Weight_Coarse,
+	'weight of TMD':Weight_TMD,
+	'compaction count':Compaction,
+	'slump result':Slump,
+	'Density of Cube':Cube_Density}
+
+features = pd.DataFrame(data, index=[0])
+
+prediction = model.predict(features)
+
+if st.button('Predict'):
+    st.write(prediction[0])
+else:
+    st.write('Click to predict')
